c91354ad63a8c18ee10fa57a912408fbb3f4ecc1
Ziopod <ziopod@gmail.com>
2020-11-16 12:25:28 +0100

    Use preset env with default configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   sitemap: true,
   plugins: [
     require('postcss-import'),
-    require('postcss-preset-env')({ stage: 0, browsers: 'last 2 versions'}),
+    require('postcss-preset-env'),
     require('cssnano')({ preset: 'default' }),
     require('postcss-csso')
   ]
